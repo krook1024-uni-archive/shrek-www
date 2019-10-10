a8ddd615bb48655f530f4629607ec36713e3b506
krook1024 <krook1024@gmail.com>
2019-10-10 16:11:47 +0200

    progress

--- logika.js
+++ logika.js
@@ -276,10 +276,10 @@ function setError() {
 function toLatex(str) {
     str = str.replace("{", "\\{");
     str = str.replace("}", "\\}");
-    str = str.replace(/\u00ac/g, "\\lnot");
-    str = str.replace(/\u02c5/g, "\\lor");
-    str = str.replace(/\u02c4/g, "\\land");
-    str = str.replace(/\u2283/g, "\\supset");
+    str = str.replace(/\u00ac/g, "\\lnot ");
+    str = str.replace(/\u02c5/g, "\\lor ");
+    str = str.replace(/\u02c4/g, "\\land ");
+    str = str.replace(/\u2283/g, "\\supset ");
     console.log(str);
     return str;
 }
